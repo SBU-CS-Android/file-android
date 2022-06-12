af46362cc1ce91cd6361c22584e1b6e0a22c7f4a
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-06-12 22:56:01 +0430

    finished file chooser tutorial

--- app/src/main/java/io/github/muhammadkarbalaee/myapplication/MainActivity.java
+++ app/src/main/java/io/github/muhammadkarbalaee/myapplication/MainActivity.java
@@ -3,67 +3,75 @@ package io.github.muhammadkarbalaee.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.ImageView;
 
-import java.util.ArrayList;
-
-import io.realm.Realm;
-import io.realm.RealmConfiguration;
-import io.realm.RealmQuery;
-import io.realm.RealmResults;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
-
-  EditText textInput;
-  Button dbButton;
-  Button sendButton;
-  Realm realmDB;
+  ImageView imageView;
+  Button pickImageButton;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    Realm.init(this);
+    pickImageButton = findViewById(R.id.button);
+    imageView = findViewById(R.id.imageView);
 
-    RealmConfiguration realmConfig = new RealmConfiguration.Builder().name("realmDB")
-        .allowQueriesOnUiThread(true)
-        .allowWritesOnUiThread(true)
-        .build();
-    realmDB = Realm.getInstance(realmConfig);
-
-
-    textInput = findViewById(R.id.editTextTextPersonName);
-    dbButton = findViewById(R.id.button);
-    sendButton = findViewById(R.id.button2);
-
-    dbButton.setOnClickListener(new Button.OnClickListener() {
+    pickImageButton.setOnClickListener(new Button.OnClickListener() {
       @Override
       public void onClick(View view) {
-        User newUser = new User(textInput.getText().toString());
-        realmDB.beginTransaction();
-        realmDB.copyToRealm(newUser);
-        realmDB.commitTransaction();
-        Toast.makeText(getApplicationContext(),"Saved to DB", Toast.LENGTH_LONG).show();
+        Intent gallery = new Intent(Intent.ACTION_GET_CONTENT);
+        gallery.setType("*/*");
+        startActivityForResult(gallery,1);
       }
     });
 
-    sendButton.setOnClickListener(new Button.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        RealmQuery<User> usersQuery = realmDB.where(User.class);
-        usersQuery.equalTo("username","hamid");
-        RealmResults<User> userResults = usersQuery.findAll();
-        //because realm objects can only be accessed at the thread they were generated
-        ArrayList<User> users = new ArrayList<>(realmDB.copyFromRealm(userResults));
-        SendServerTask sendServerTask = new SendServerTask(users);
-        sendServerTask.execute();
-        Toast.makeText(getApplicationContext(),"Sent to server", Toast.LENGTH_LONG).show();
-      }
-    });
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode,Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+      Uri imageUri = data.getData();
+      imageView.setImageURI(imageUri);
+      loadImage(imageUri);
+    }
+  }
+
+  private void loadImage(Uri imageUri) {
+    try {
+      InputStream inputStream = this.getContentResolver().openInputStream(imageUri);
+      byte[] fileByte = new byte[inputStream.available()];
+      System.out.println("chosen image file size:   " + inputStream.available());
+      inputStream.read(fileByte);
+      inputStream.close();
+      saveImage(fileByte);
+      loadFromFolder();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void loadFromFolder() throws IOException {
+    FileInputStream fileInputStream = openFileInput("image.png");
+    System.out.println("new image file size:   " + fileInputStream.available());
+  }
+
+  private void saveImage(byte[] fileByte) throws IOException {
+    FileOutputStream fileOutputStream = openFileOutput("image.png", MODE_PRIVATE);
+    fileOutputStream.write(fileByte);
+    fileOutputStream.flush();
+    fileOutputStream.close();
   }
 }
